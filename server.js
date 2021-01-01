03678ad8e163f10b7c4bed9f63ef827523b9aa05
Serghei Mihailov <mihailov.serghei.d@gmail.com>
2021-01-01 17:29:44 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1Ynr4olX0gHgTpdTEq3AjOmtdhBCq5aAxcej-JL4vi0Y';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -19,18 +19,41 @@ async function onGet(req, res) {
   const rows = result.rows;
   console.log(rows);
 
-  // TODO(you): Finish onGet.
+  const objArray = [];
+  for (let rowIndex = 1; rowIndex < rows.length; rowIndex++) {
+    let newEntry = {};
+    for (let colIndex = 0; colIndex < rows[0].length; colIndex++) {
+      newEntry[rows[0][colIndex]] = rows[rowIndex][colIndex];
+    }
+    objArray.push(newEntry);
+  }
 
-  res.json( { status: 'unimplemented'} );
+  res.json( objArray );
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
+  const preRows = await sheet.getRows();
+  const rows = preRows.rows;
+  const docArray = [];
 
-  // TODO(you): Implement onPost.
+  for (const field in messageBody) {
+    docArray[field] = messageBody[field];
+  }
 
-  res.json( { status: 'unimplemented'} );
+  const payload = [];
+  for (field in docArray) {
+    const fieldIndex = rows[0].findIndex((element) => {
+      return element === field;
+    });
+    payload[fieldIndex] = docArray[field];
+  }
+
+  console.log(docArray);
+  console.log(payload);
+  const result = await sheet.appendRow(payload);
+  res.json(result);
 }
 app.post('/api', jsonParser, onPost);
 
@@ -38,20 +61,52 @@ async function onPatch(req, res) {
   const column  = req.params.column;
   const value  = req.params.value;
   const messageBody = req.body;
+  const preRows = await sheet.getRows();
+  const rows = preRows.rows;
+  const docArray = [];
 
   // TODO(you): Implement onPatch.
+  const colIndex = rows[0].findIndex((element, index, array) => {
+    return element === column;
+  });
+  const indexToPatch = rows.findIndex((element, index, array) => {
+      return element[colIndex] === value;
+    }
+  );
 
-  res.json( { status: 'unimplemented'} );
+  for (const field in messageBody) {
+    docArray[field] = messageBody[field];
+  }
+
+  const payload = [];
+  for (field in docArray) {
+    const fieldIndex = rows[0].findIndex((element) => {
+      return element === field;
+    });
+    payload[fieldIndex] = docArray[field];
+  }
+
+  const result = await sheet.setRow(indexToPatch, payload);
+
+  res.json(result);
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
   const column  = req.params.column;
   const value  = req.params.value;
+  const getRows = await sheet.getRows();
+  const rows = getRows.rows;
 
-  // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+  const colIndex = rows[0].findIndex((element, index, array) => {
+    return element === column;
+  });
+  const indexToDelete = rows.findIndex((element, index, array) => {
+      return element[colIndex] === value;
+    }
+  );
+  const result = await sheet.deleteRow(indexToDelete);
+  res.json(result);
 }
 app.delete('/api/:column/:value',  onDelete);
 
